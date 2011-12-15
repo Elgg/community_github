aa6089bd41987a224abe3ea8d069b6291c7408bb
cash <cash.costello@gmail.com>
2011-12-14 22:45:02 -0500

    added hook to pull data from github

--- start.php
+++ start.php
@@ -6,9 +6,12 @@
 register_elgg_event_handler('init', 'system', 'community_github_init');
 
 function community_github_init() {
+	// add github username to profile
 	register_plugin_hook('profile:fields', 'profile', 'community_github_profile');
-
 	elgg_extend_view('profile/userdetails', 'github/hack', 1);
+
+	// do a regular pull of github information
+	register_plugin_hook('cron', 'hourly', 'community_github_pull');
 }
 
 /**
@@ -22,4 +25,11 @@ function community_github_init() {
 function community_github_profile($hook, $type, $fields) {
 	$fields['github_username'] = 'github';
 	return $fields;
+}
+
+/**
+ * Pull the latest information from github
+ */
+function community_github_pull() {
+
 }
